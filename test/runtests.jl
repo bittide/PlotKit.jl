09cafd820df8b84ad3b7235ddc15830e81455662
Sanjay Lall <sanjaylall@google.com>
2024-01-29 14:54:11 -0800

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,13 @@ using PlotKitCairo
 using PlotKitAxes
 using PlotKitDiagrams
 using PlotKitGL
+using Cairo
 
 
-include(joinpath(pkgdir(PlotKitCairo), "test/testset.jl"))
+include(joinpath(pkgdir(PlotKitCairo), "test/runtests.jl"))
+include(joinpath(pkgdir(PlotKitAxes), "test/runtests.jl"))
+include(joinpath(pkgdir(PlotKitDiagrams), "test/runtests.jl"))
+include(joinpath(pkgdir(PlotKitGL), "test/runtests.jl"))
 
 
 
