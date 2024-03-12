960974828daf9c80ff5bcbe6520ceba83dd06449
Sanjay Lall <lall@stanford.edu>
2024-03-12 16:14:32 -0700

    minor

--- src/PlotKit.jl
+++ src/PlotKit.jl
@@ -21,7 +21,7 @@ reexport(PlotKitCharts)
 abstract type Extension end
 export setup3d
 setup3d(a::Extension) = println("Hello")
-fnames = [:mesh_height_color_fn, :mesh_height_fn, :mesh]
+fnames = [:mesh_height_color_fn, :mesh_height_fn, :mesh, :surf]
 for f in fnames
     @eval $f(a::Extension) = return
     @eval export $f
