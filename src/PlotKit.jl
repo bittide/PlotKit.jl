d64225d16ba8501ffa44b547a16ef55c7a959ff9
Sanjay Lall <sanjaylall@google.com>
2024-03-15 16:32:46 -0700

    minor

--- src/PlotKit.jl
+++ src/PlotKit.jl
@@ -19,13 +19,18 @@ reexport(PlotKitCharts)
 
 # For extensions
 abstract type Extension end
-export setup3d
-setup3d(a::Extension) = println("Hello")
-fnames = [:mesh_height_color_fn, :mesh_height_fn, :mesh, :surf]
+abstract type Shape end
+abstract type Texture end
+abstract type Uniform <: Texture end
+abstract type Ellipsoid <: Shape end
+abstract type Box3 end
+
+fnames = [:mesh_height_color_fn, :mesh_height_fn, :mesh, :surf, :Uniform, :Ellipsoid, :Box3, :raytrace]
 for f in fnames
     @eval $f(a::Extension) = return
     @eval export $f
 end
+
 
 
 
