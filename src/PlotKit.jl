73b59f9399b771252205a9787b2049476f733431
Sanjay Lall <lall@stanford.edu>
2024-03-19 19:53:56 -0700

    minor

--- src/PlotKit.jl
+++ src/PlotKit.jl
@@ -21,7 +21,7 @@ reexport(PlotKitCharts)
 abstract type Extension end
 
 fnames = [:mesh_height_color_fn, :mesh_height_fn, :mesh, :surf, :raytrace, :ellipsoid, :box3,
-          :uniform, :polytope, :arbitrarysolid, :vec3]
+          :uniform, :polytope, :arbitrarysolid, :vec3, :pk3d]
 for f in fnames
     @eval $f(a::Extension) = return
     @eval export $f
